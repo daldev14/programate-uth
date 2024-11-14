35a919a8e8c4c70f4ead55bf158223da0466ff62
Daniel Amaya <damayalop@gmail.com>
2024-11-14 14:22:26 -0600

    Update courses

--- src/courses.ts
+++ src/courses.ts
@@ -5,7 +5,7 @@ export const COURSES = [
     name: "Programación para todos (Introducción a Python)",
     subtitle: "Programación para todos (Introducción a Python)",
     certificateURL:
-      "https://drive.google.com/file/d/1CelgU8KSK5yIyW2d3suE7mZNjoIB0SbW/view?usp=drive_link",
+      "https://drive.google.com/file/d/1CelgU8KSK5yIyW2d3suE7mZNjoIB0SbW/preview",
     stage: 1,
     url: "https://www.coursera.org/programs/programate-pyvbi/learn/python-es",
     iconName: CourseIcon.python,
@@ -14,7 +14,7 @@ export const COURSES = [
     name: "Aprendiendo Python con álgebra lineal",
     subtitle: "Aprendiendo Python con álgebra lineal",
     certificateURL:
-      "https://drive.google.com/file/d/1oMg7UreZMfryBPbJg9HIFSih7oYzfj3E/view?usp=drive_link",
+      "https://drive.google.com/file/d/1oMg7UreZMfryBPbJg9HIFSih7oYzfj3E/preview",
     stage: 1,
     url: "https://www.coursera.org/programs/programate-pyvbi/projects/aprendiendo-python-con-algebra-lineal",
     iconName: CourseIcon.python,
@@ -23,7 +23,7 @@ export const COURSES = [
     name: "Aprendiendo Python con circuitos digitales",
     subtitle: "Aprendiendo Python con circuitos digitales",
     certificateURL:
-      "https://drive.google.com/file/d/1-EGosugbb_fUtUlpwoBhLbP4xuI12Jsd/view?usp=drive_link",
+      "https://drive.google.com/file/d/1-EGosugbb_fUtUlpwoBhLbP4xuI12Jsd/preview",
     stage: 1,
     url: "https://www.coursera.org/programs/programate-pyvbi/projects/aprendiendo-python-con-circuitos-digitales",
     iconName: CourseIcon.python,
@@ -32,7 +32,7 @@ export const COURSES = [
     name: "Aprendiendo Python con textos, números y ecuaciones",
     subtitle: "Aprendiendo Python con textos, número y ecuaciones",
     certificateURL:
-      "https://drive.google.com/file/d/1rthJwssIrjf5E7Vr7ikyavX4hxa190UE/view?usp=drive_link",
+      "https://drive.google.com/file/d/1rthJwssIrjf5E7Vr7ikyavX4hxa190UE/preview",
     stage: 1,
     url: "https://www.coursera.org/programs/programate-pyvbi/projects/aprendiendo-python-con-textos-numeros-ecuaciones",
     iconName: CourseIcon.python,
@@ -42,7 +42,7 @@ export const COURSES = [
     subtitle:
       "Introducción a la programación orientada a objetos en Java: Continuación de los conceptos básicos de programación, pero con un enfoque en la orientación a objetos",
     certificateURL:
-      "https://drive.google.com/file/d/1tEY81V892tKKu9W_MbKwEaouCWu5r7kC/view?usp=drive_link",
+      "https://drive.google.com/file/d/1tEY81V892tKKu9W_MbKwEaouCWu5r7kC/preview",
     stage: 1,
     url: "https://www.coursera.org/programs/programate-pyvbi/learn/introduccion-programacion-java",
     iconName: CourseIcon.java,
@@ -52,7 +52,7 @@ export const COURSES = [
     subtitle:
       "Empezando a trabajar con VueJS: Introducción al desarrollo frontend con Vue.js",
     certificateURL:
-      "https://drive.google.com/file/d/1q_aRlkaCw6Y31WnVnYQ_gZh5MSACzmkt/view?usp=drive_link",
+      "https://drive.google.com/file/d/1q_aRlkaCw6Y31WnVnYQ_gZh5MSACzmkt/preview",
     stage: 2,
     url: "https://www.coursera.org/programs/programate-pyvbi/projects/crea-una-aplicacion-web-con-vue-js",
     iconName: CourseIcon.vuejs,
@@ -62,7 +62,7 @@ export const COURSES = [
     subtitle:
       "Agrega estilo a una página HTML usando CSS: Fundamentos de estilización y diseño web",
     certificateURL:
-      "https://drive.google.com/file/d/1MIVbYVKsvLONwL17uhWtTxdDbShZa7R-/view?usp=drive_link",
+      "https://drive.google.com/file/d/1MIVbYVKsvLONwL17uhWtTxdDbShZa7R-/preview",
     stage: 2,
     url: "https://www.coursera.org/programs/programate-pyvbi/projects/agrega-estilo-pagina-html-usando-css",
     iconName: CourseIcon.python,
@@ -72,7 +72,7 @@ export const COURSES = [
     subtitle:
       "Crea una página web interactiva con JavaScript: Introducción a la interactividad en la web con JavaScript",
     certificateURL:
-      "https://drive.google.com/file/d/1XMT3Dl5sanHPd8dLM86O8Bhctk2nqzEP/view?usp=drive_link",
+      "https://drive.google.com/file/d/1XMT3Dl5sanHPd8dLM86O8Bhctk2nqzEP/preview",
     stage: 2,
     url: "https://www.coursera.org/programs/programate-pyvbi/projects/crea-pagina-web-interactiva-javascript",
     iconName: CourseIcon.javascript,
@@ -82,7 +82,7 @@ export const COURSES = [
     subtitle:
       "Creando tu primera App con ReactJS: Introducción al desarrollo de aplicaciones frontend con React.js",
     certificateURL:
-      "https://drive.google.com/file/d/1tHht6ZeUxJHliMV5PB8imp2tAPtlyLGS/view?usp=drive_link",
+      "https://drive.google.com/file/d/1tHht6ZeUxJHliMV5PB8imp2tAPtlyLGS/preview",
     stage: 2,
     url: "https://www.coursera.org/programs/programate-pyvbi/projects/creando-primera-app-reactjs",
     iconName: CourseIcon.react,
@@ -92,7 +92,7 @@ export const COURSES = [
     subtitle:
       "Introduccion a Base de Datos para el desarrollo de Backend: fundamentos de bases de datos para el desarrollo backend",
     certificateURL:
-      "https://drive.google.com/file/d/1eFkMzoX5_qCRqTWuLzu7iREqoCC4wEJb/view?usp=drive_link",
+      "https://drive.google.com/file/d/1eFkMzoX5_qCRqTWuLzu7iREqoCC4wEJb/preview",
     stage: 3,
     url: "https://www.coursera.org/programs/programate-pyvbi/learn/intro-to-databases-back-end-development-es",
     iconName: CourseIcon.database,
@@ -101,7 +101,7 @@ export const COURSES = [
     name: "Node.js backend básico con buenas prácticas",
     subtitle: "Node.js backend básico con buenas prácticas",
     certificateURL:
-      "https://drive.google.com/file/d/12FLMV6-AzitdqoChyuldiIcsaPBJzBg4/view?usp=drive_link",
+      "https://drive.google.com/file/d/12FLMV6-AzitdqoChyuldiIcsaPBJzBg4/preview",
     stage: 3,
     url: "https://www.coursera.org/programs/programate-pyvbi/projects/nodejs-backend-basico-con-buenas-practicas",
     iconName: CourseIcon.nodejs,
@@ -111,7 +111,7 @@ export const COURSES = [
     subtitle:
       "React: Carrito de compras con Context: Integración de React.js con un backend utilizando context API",
     certificateURL:
-      "https://drive.google.com/file/d/1sf16axOneat1zeja85CDH96U23Bl1vF_/view?usp=drive_link",
+      "https://drive.google.com/file/d/1sf16axOneat1zeja85CDH96U23Bl1vF_/preview",
     stage: 4,
     url: "https://www.coursera.org/programs/programate-pyvbi/projects/react-checkout-context",
     iconName: CourseIcon.react,
@@ -121,7 +121,7 @@ export const COURSES = [
     subtitle:
       "React: Operaciones Básicas CRUD y Router dinámico con API: Prácticas de integración frontend y backend con operaciones CRUD y rutas dinámicas",
     certificateURL:
-      "https://drive.google.com/file/d/1FYKs0c-CbcKT0-KE_2I4mTGXHhk11wLf/view?usp=drive_link",
+      "https://drive.google.com/file/d/1FYKs0c-CbcKT0-KE_2I4mTGXHhk11wLf/preview",
     stage: 4,
     url: "https://www.coursera.org/programs/programate-pyvbi/projects/react-crud",
     iconName: CourseIcon.react,
@@ -131,7 +131,7 @@ export const COURSES = [
     subtitle:
       "Uso de Python para Acceder a Datos Web: Introducción a la automatización de tareas y acceso a datos web con Python",
     certificateURL:
-      "https://drive.google.com/file/d/1rES9nyIRgtsQpBvPRK5gDB59VnRU3nwi/view?usp=drive_link",
+      "https://drive.google.com/file/d/1rES9nyIRgtsQpBvPRK5gDB59VnRU3nwi/preview",
     stage: 4,
     url: "https://www.coursera.org/programs/programate-pyvbi/learn/python-network-data-es",
     iconName: CourseIcon.python,
@@ -140,7 +140,7 @@ export const COURSES = [
     name: "Build a Machine Learning Web App with Streamlit and Python",
     subtitle: "Build a Machine Learning Web App with Streamlit and Python",
     certificateURL:
-      "https://drive.google.com/file/d/1i5r2T9-xoqj3rLK4UQ6BY7eK7-OlIdT3/view?usp=drive_link",
+      "https://drive.google.com/file/d/1i5r2T9-xoqj3rLK4UQ6BY7eK7-OlIdT3/preview",
     stage: 4,
     url: "https://www.coursera.org/programs/programate-pyvbi/projects/machine-learning-streamlit-python",
     iconName: CourseIcon.python,
@@ -159,7 +159,7 @@ export const COURSES = [
     subtitle:
       "Desarrollo de software Agile: Fundamentos de metodologías ágiles",
     certificateURL:
-      "https://drive.google.com/file/d/1J1CfF_NyXwmHd-JX30C01OwUua5ylWYW/view?usp=drive_link",
+      "https://drive.google.com/file/d/1J1CfF_NyXwmHd-JX30C01OwUua5ylWYW/preview",
     stage: 5,
     url: "https://www.coursera.org/programs/programate-pyvbi/learn/agile-software-development-es",
     iconName: CourseIcon.python,
@@ -169,7 +169,7 @@ export const COURSES = [
     subtitle:
       "Introducción a Git y GitLab: Control de versiones y colaboración en proyectos de desarrollo de software",
     certificateURL:
-      "https://drive.google.com/file/d/1PvvJmuv_uATx9e3I2u0hY3Er4FCqdRH2/view?usp=drive_link",
+      "https://drive.google.com/file/d/1PvvJmuv_uATx9e3I2u0hY3Er4FCqdRH2/preview",
     stage: 5,
     url: "https://www.coursera.org/programs/programate-pyvbi/projects/intro-git-gitlab",
     iconName: CourseIcon.git,
@@ -178,7 +178,7 @@ export const COURSES = [
     name: "Trabajando de inicio a fin con GIT",
     subtitle: "Bases y primeros pasos con Git",
     certificateURL:
-      "https://drive.google.com/file/d/1dSRjsgdvauaEZDgh-qB50KNDzGaVli2b/view?usp=drive_link",
+      "https://drive.google.com/file/d/1dSRjsgdvauaEZDgh-qB50KNDzGaVli2b/preview",
     stage: 5,
     url: "https://www.coursera.org/programs/programate-pyvbi/projects/git-inicio-fin",
     iconName: CourseIcon.git,
@@ -188,7 +188,7 @@ export const COURSES = [
     subtitle:
       "Introducción a la terminal de comandos de Linux: Fundamentos de la línea de comandos en sistemas Linux",
     certificateURL:
-      "https://drive.google.com/file/d/1ko18cwN4pCqn5oPr1x-y5MCrmxPt5K1K/view?usp=drive_link",
+      "https://drive.google.com/file/d/1ko18cwN4pCqn5oPr1x-y5MCrmxPt5K1K/preview",
     stage: 5,
     url: "https://www.coursera.org/programs/programate-pyvbi/projects/introduccion-a-la-terminal-de-comandos-de-linux",
     iconName: CourseIcon.bash,
